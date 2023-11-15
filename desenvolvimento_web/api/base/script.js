a4dc407c8b0bf45f7eb45e4b2dbf8c4f39bce315
zander404 <alexandrearthurpg@gmail.com>
2023-11-15 11:24:06 -0300

    setando base

---
+++ desenvolvimento_web/api/base/script.js
@@ -0,0 +1,21 @@
+// Pegar nossos campos do HTML 
+
+// Instancia uma variavel para puxar o primeiro pokemon
+let searchPokemon = 1;
+
+
+// Consultar a API e retorna a resposta da requisição e retornar os dados de sua resposta
+const fetchPokemon = ""
+
+// Pegar as informacoes que desejamos e colocar em nossas variaveis
+const renderPokemon = ""
+
+// Pesquisar pelo Nome/Numero do Pokemon 
+form.addEventListener("submit", ()=>{ })
+
+
+// Botão de Voltar a Lista
+prev.addEventListener("click", ()=>{ })
+
+// Botão de Proximo na Lista
+next.addEventListener("click", ()=>{ })
