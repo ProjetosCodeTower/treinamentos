324c8d8287959601e9a89972a424ff49f951c9a5
zander404 <alexandrearthurpg@gmail.com>
2023-11-16 18:19:20 -0300

    minor

--- desenvolvimento_web/api/base/script.js
+++ desenvolvimento_web/api/base/script.js
@@ -1,21 +1,31 @@
-// Pegar nossos campos do HTML 
+// Pegar nossos campos do HTML
+const pokemonName = document.querySelector(".pokemon__name");
+const pokemonNumber = document.querySelector(".pokemon__number");
+const pokemonImage = document.querySelector(".pokemon__image");
+const input = document.querySelector(".input__search");
+const next = document.querySelector(".btn-next");
+const prev = document.querySelector(".btn-prev");
+const form = document.querySelector(".form");
 
 // Instancia uma variavel para puxar o primeiro pokemon
 let searchPokemon = 1;
 
-
 // Consultar a API e retorna a resposta da requisição e retornar os dados de sua resposta
-const fetchPokemon = ""
+const fetchPokemon = async (pokemon) => {
+}
 
 // Pegar as informacoes que desejamos e colocar em nossas variaveis
-const renderPokemon = ""
+const renderPokemon = async(pokemon) => {}
 
-// Pesquisar pelo Nome/Numero do Pokemon 
-form.addEventListener("submit", ()=>{ })
-
+renderPokemon(1);
+// Pesquisar pelo Nome/Numero do Pokemon
+form.addEventListener("submit", () => {});
 
 // Botão de Voltar a Lista
-prev.addEventListener("click", ()=>{ })
+prev.addEventListener("click", () => {
+});
 
 // Botão de Proximo na Lista
-next.addEventListener("click", ()=>{ })
+next.addEventListener("click", () => {
+
+});
